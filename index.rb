a4ccc67aa40433d383b45a069572237de97f5e37
Kaung Khant Zaw <kaung34khant@gmail.com>
2022-04-01 16:39:55 +0630

    comparison if statement

--- index.rb
+++ index.rb
@@ -1,3 +1,5 @@
+# testing method 
+
 # def outputInfo(name,age)
 #     puts "Your name is " + name + " your age is "+ age
 # end
@@ -12,12 +14,42 @@
 # end
 # output("Kaung Khant Zaw",20)
 
-def fourth(num)
-    result = num ** 4
-    puts "Hello this is under return statement"
-    return result
-end
-puts "enter a number : "
-num = gets.chomp()
-puts fourth(num.to_f)
+# method with return statement
 
+# def fourth(num)
+#     result = num ** 4
+#     puts "Hello this is under return statement"
+#     return result
+# end
+# puts "enter a number : "
+# num = gets.chomp()
+# puts fourth(num.to_f)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#if statement 
+
+# isRuby = false
+# isPython = false
+
+# if isRuby and isPython
+#     puts "It is ruby and python"
+# elsif isRuby and !isPython
+#     puts "It is ruby but not python"
+# elsif !isRuby and isPython
+#     puts "It is not ruby but is python"
+# else  
+#     puts "It is neither ruby nor python"
+# end 
+
+def min(num1 ,num2 , num3)
+    if(num1 <= num2 and num1 <= num3)
+        return num1
+    elsif (num2 <= num1 and num2 <= num3)
+        return num2
+    else
+        return num3
+    end
+end
+
+puts min(-2,1,3)
